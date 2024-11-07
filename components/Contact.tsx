2c46ac0cb7cbf7b67bfa89d2f0b2ba9aa05ac8f4
Muhammad Usman <160177186+UsmanRaees@users.noreply.github.com>
2024-11-07 14:16:07 +0500

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,14 +8,14 @@ const Contact = () => {
         <div className='grid md:grid-cols-2 gap-10'>
             <div className='space-y-8'>
                 <h2 className='text-5xl' data-aos="zoom-in-up">Get in Touch</h2>
-                <p className='text-gray-700 text-[18px] pt-2' data-aos="zoom-in-up">
+                <p className='text-gray-700 text-[18px] pt-2 font-bold' data-aos="zoom-in-up">
                     Drop me a line, give me a call, or send me a message by submitting the form.
                 </p>
                 <div className='flex gap-3 items-center' data-aos="zoom-in-up">
-                    <FiMail size={30} /> m.usmanraees21@gamil.com
+                    <FiMail className='text-red-700' size={30} /> m.usmanraees21@gamil.com
                 </div>
                 <div className='flex gap-3 items-center'  data-aos="zoom-in-up">
-                    <BsTelephonePlus size={30} /> (+92) 3350386812
+                    <BsTelephonePlus className='text-sky-950' size={30} /> (+92) 3350386812
                 </div>
             </div>
             <div className='space-y-8'>
@@ -31,7 +31,7 @@ const Contact = () => {
                     <label htmlFor="msg">Message</label>
                     <textarea className='bg-transparent border border-accent' id='msg' rows={8}></textarea>
                 </div>
-                <button className='bg-accent p-2 px-6' data-aos="zoom-in-up">Send</button>
+                <button className='bg-accent p-2 px-6 rounded-md' data-aos="zoom-in-up">Send</button>
             </div>
         </div>
     </div>
